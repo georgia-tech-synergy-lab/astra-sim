d6c9d35f52f285b06e02a8fccebe77b97f0e9ae1
System Administrator <root@ipsec-172-16-72-28.vpn.gatech.edu>
2020-03-14 17:58:44 -0700

    increased readibility

--- system/Sys.cc
+++ system/Sys.cc
@@ -1,5 +1,5 @@
 /******************************************************************************
-Copyright (c) 2020 Georgia Instititue of Technology
+Copyright (c) 2020 Georgia Institute of Technology
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
